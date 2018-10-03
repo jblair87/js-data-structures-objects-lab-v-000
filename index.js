58d6a28e0149b4f496eee42f70cd6fd60b8a8316
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-03 21:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const driver = {};
 updateDriverWithKeyAndValue(driver, key, value){
-  return Object.assign({}, driver, { [key]: value });
+  return Object.assign({}, driver, { [name]: "Sam" });
 }
 destructivelyUpdateDriverWithKeyAndValue(){}
 deleteFromDriverByKey(){}
